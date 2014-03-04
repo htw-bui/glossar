ef3a0305b9415f54715062ad56fc71f031706e62
Knut Hühne <knut@k-nut.eu>
2014-03-04 17:15:56 +0100

    Added grunt

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		//pkg: grunt.file.readJSON('package.json'),
+		watch:{
+			options: {
+				livereload: true
+			},
+			stylesheets: {
+				files: ["sass/*"],
+				tasks: ["sass"]
+			}
+		},
+		sass: {
+			dev: {
+				options: {
+					style: "expanded"
+				},
+				files: {
+					"css/main.css" : "sass/main.sass"
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-sass'); 
+
+	// Default task(s).
+	grunt.registerTask('default', ['sass', 'watch']);
+
+};
