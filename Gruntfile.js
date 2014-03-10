eb86a775faa884a6d350bf3a672fa2026a9f5ce3
Knut Hühne <knut@k-nut.eu>
2014-03-10 14:37:22 +0100

    Add grunt modules for creating compact dist build

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,25 +27,73 @@ module.exports = function(grunt) {
 				files: {
 					"css/main.css" : "sass/main.sass"
 				}
+			},
+			dist: {
+				options: {
+					style: "compressed"
+				},
+				files: {
+					"css/main.css" : "sass/main.sass"
+				}
 			}
+
 		},
 		autoprefixer: {
-
 			maincss: {
 				options: {
 					diff: true
-					},
-					src: "css/main.css",
-					dest: "dist/css/main.css"
+				},
+				src: "css/main.css",
+				dest: "dist/css/main.css"
+			},
+			dist: {
+				src: "dist/css/main.css",
+				dest : "css/main.css"
+			}
+		},
+		htmlmin: {
+			dist: {
+				options: {
+					collapseWhitespace: true,
+					removeRedundantAttributes: true,
+					removeAttributQuotes: true,
+					collapseBooleanAttribute: true
+				},
+				files: {
+					'dist/index.html': 'index.html'
+				}
+			}
+		},
+		copy: {
+			dist: {
+				src:"neu_generierte_begriffe.json", 
+				dest:"dist/neu_generierte_begriffe.json"
+			}
+		},
+		uglify: {
+			dist: { 
+				options: {
+					mangle: true
+				},
+				files: {
+					"dist/main.js": "main.js"
+				}
 			}
 		}
+
+
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-sass'); 
 	grunt.loadNpmTasks('grunt-autoprefixer'); 
+	grunt.loadNpmTasks('grunt-contrib-htmlmin');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 
-	// Default task(s).
+
+
 	grunt.registerTask('default', ['sass', 'watch']);
+	grunt.registerTask('build', ['sass:dist', "autoprefixer:dist", "htmlmin:dist", "copy:dist", "uglify:dist"]);
 
 };
