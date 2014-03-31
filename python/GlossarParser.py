726a0fd184b0fe335f82554ed8fa2b2caf38c6ee
Knut Hühne <knut@k-nut.eu>
2014-03-31 15:32:07 +0200

    censor out terms in pyzhon

--- python/GlossarParser.py
+++ python/GlossarParser.py
@@ -2,6 +2,7 @@ import json
 import codecs
 import collections
 import os
+import re
 
 
 def merge():
@@ -47,7 +48,30 @@ def generate_from_merged(withlinks=True):
     with open(outpath, "w") as out:
         out.write(json.dumps(od, indent=2))
 
+
+def censor():
+    fullpath = os.path.dirname(os.path.abspath(__file__)) + "/"
+
+    path = fullpath + '../data/merged-nolinks.html'
+    outpath = fullpath + '../data/censored.html'
+
+    with open(path, "r", encoding="utf-8") as infile:
+        content = infile.read().split("---------")
+
+    p = []
+    for part in content:
+        parts = part.split("=========")
+        term = parts[0].strip()
+        synonyms = parts[1].split(",")
+        description = parts[2].strip().replace("\n", " ")
+        description = re.sub('(?i)(\w*)' + term + '(\w*)', r'\1xxxx\2', description)
+        p.append("%s\n=========\n%s=========\n%s\n\n" % (term, " ".join(synonyms),
+                                                         description))
+    with open(outpath, "w") as out:
+        out.write("---------\n".join(p))
+
 if __name__ == '__main__':
-    generate_from_merged()
-    generate_from_merged(withlinks=False)
+    #generate_from_merged()
+    #generate_from_merged(withlinks=False)
+    censor()
 
