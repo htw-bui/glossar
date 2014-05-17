6779786d05f593dceaf55b769d62d1922a7c201c
Knut Hühne <knut@k-nut.eu>
2014-05-17 15:05:28 +0200

    fix number being shown py progresscounter

--- js/game.js
+++ js/game.js
@@ -32,7 +32,7 @@ define(['jquery', 'ProgressCounter', 'stopwatch', 'utils'], function($, Progress
 
 
   function initializeObjects() { 
-    progressCounter = new ProgressCounter(keys.length);
+    progressCounter = new ProgressCounter(keys.length+1);
     progressCounter.onChange = function(){
       var progress = $("<div>", {
         text: this.numberOfTermsRead() + '|' + this.numberOfTerms
