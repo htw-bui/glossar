5100a4d28fb1284e8610a654330065de015e6b69
Knut Hühne <knut@k-nut.eu>
2014-10-21 15:25:07 +0200

    use angular for routing

--- src/js/app.js
+++ src/js/app.js
@@ -1,121 +1,42 @@
 (function(){
   "use strict";
-  var app = angular.module("dashboard", []);
+  var app = angular.module("dashboard", [
+    "ngRoute",
+    "kometControllers"
+  ]);
 
-
-  app.directive("navigationList", function(){
-    return {
-      restrict: "A",
-      templateUrl: "navigation-list.html"
-    };
-  });
-
-  app.directive("topNavigation", function(){
-    return {
-      restrict: "A",
-      templateUrl: "top-navigation.html"
-    };
-  });
-
-  app.directive("glossaryControl", function(){
-    return {
-      restrict: "A",
-      templateUrl: "glossary.html"
-    };
-  });
-
-  app.directive("highscoreView", function(){
-    return {
-      restrict: "E",
-      templateUrl: "highscore.html",
-      controller: function($scope, $http){
-        $scope.highscores = {};
-        $http.get("http://highscore.k-nut.eu/highscore")
-          .then(function(res){
-            $scope.highscores = res.data;
-            window.s = res.data;
-          });
-          $scope.calculateMean = function (score){
-            score = score.score; // TODO naming
-            return Math.round((score.time / 1000) / score.score * 10) / 10;
-          };
-          $scope.getMonthAndYear = function(date){
-            moment.lang("de");
-            return moment(date).format("MMMM YYYY");
-          };
-          $scope.parseDate = function(date){
-            return Date.parse(date);
-          };
-      }
-    };
-  });
-
-  app.controller("DashboardController" , function($scope, $http){
-    $scope.tab = 2;
-    $scope.searchTerm = "";
-    $scope.terms = [{"term": "dummy"}];
-    $scope.selectedTerm = {};
-    $scope.progressCounter = {};
-    $scope.stopWatch = {};
-
-    $http.get("/data/terms-en.json")
-    .then(function(res){
-      $scope.terms = res.data;
-      $scope.selectedTerm = res.data[0];
-      $scope.progressCounter = new ProgressCounter(res.data.length);
-    }).then(loadTermFromHash);
-
-    $scope.filter = function(term){
-      return term.term.toLowerCase().contains($scope.searchTerm.toLowerCase());
-    };
-
-    $scope.setSelectedItem = function(term){
-      setSelectedTerm(term.term);
-    };
-
-    $scope.resetProgress = function(){
-      $scope.progressCounter.clear();
-    };
-
-    function setSelectedTerm(term){
-      $scope.progressCounter.registerTerm(term.term);
-      $scope.selectedTerm = term;
-      window.location.hash = term.term;
-    }
-
-
-    $scope.prevTerm = function(){
-      var indexOfSelectedTerm = getIndexOfSelectedTerm();
-      setSelectedTerm($scope.terms[indexOfSelectedTerm - 1]);
-    };
-
-    $scope.nextTerm = function(){
-      var indexOfSelectedTerm = getIndexOfSelectedTerm();
-      setSelectedTerm($scope.terms[indexOfSelectedTerm + 1]);
-    };
-
-    $scope.selectedTermIsFirstTerm = function(){
-      return $scope.terms[0].term === $scope.selectedTerm.term;
-    };
-
-    $scope.selectedTermIsLastTerm = function(){
-      return _.last($scope.terms).term === $scope.selectedTerm.term;
-    };
-
-
-    function getIndexOfSelectedTerm(){
-      return _.findIndex($scope.terms, function(term){
-        return $scope.selectedTerm.term === term.term;
+  app.config(['$routeProvider',
+    function($routeProvider) {
+      $routeProvider.
+      when('/glossary', {
+        templateUrl: 'glossary.html',
+        controller: 'DashboardController',
+        reloadOnSearch: false
+      }).
+      when('/highscore', {
+        templateUrl: 'highscore.html',
+        controller: "HighscoreCtrl"
+        }).
+      otherwise({
+        redirectTo: '/highscore'
       });
-    }
+    }]);
 
-    function loadTermFromHash(){
-      if(window.location.hash){
-        $scope.selectedTerm = _.find($scope.terms, function(term){
-          return term.term === window.location.hash.substring(1);
-        });
-      }
-    }
-  });
+
+    app.directive("navigationList", function(){
+      return {
+        restrict: "A",
+        templateUrl: "navigation-list.html"
+      };
+    });
+
+    app.directive("topNavigation", function(){
+      return {
+        restrict: "A",
+        templateUrl: "top-navigation.html"
+      };
+    });
+
+
 
 })();
