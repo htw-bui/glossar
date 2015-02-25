368c38bda6c27be7cd9d33ddf5694ad1b2248144
Knut Hühne <knut@k-nut.eu>
2015-02-25 18:47:07 +0100

    Indentation

--- src/js/dashboardCtrl.js
+++ src/js/dashboardCtrl.js
@@ -23,81 +23,81 @@ angular.module("komet.controllers").controller("DashboardController", ["$scope",
     $scope.progressCounter = new ProgressCounter("glossary", res.data.length);
   }).then(loadTermFromHash);
 
-$scope.filter = function(term){
-  var selectedLanguage = $translate.use();
-  var property = selectedLanguage === "en" ? "term-english" : "term-german";
-  return term[property].toLowerCase().contains($scope.searchTerm.toLowerCase());
-};
+  $scope.filter = function(term){
+    var selectedLanguage = $translate.use();
+    var property = selectedLanguage === "en" ? "term-english" : "term-german";
+    return term[property].toLowerCase().contains($scope.searchTerm.toLowerCase());
+  };
 
 
-$scope.changeLang = function (newKey) {
-  var key;
-  if (newKey){
-    key = newKey;
+  $scope.changeLang = function (newKey) {
+    var key;
+    if (newKey){
+      key = newKey;
+    }
+    else{
+      key = $translate.use() === "de" ? "en" : "de";
+    }
+    localStorage.setItem("language", key);
+    $translate.use(key);
+    $scope.selectedLang = key;
+    bootbox.setLocale(key);
+    moment.locale(key);
+  };
+
+  $scope.setSelectedItem = function(term){
+    setSelectedTerm(term.term);
+  };
+
+  $scope.toggleNavigation = function(){
+    $scope.navOpen = !$scope.navOpen;
+  };
+
+  $scope.isActive = function(path){
+    return $location.path() === path;
+  };
+
+  $scope.resetProgress = function(){
+    $scope.progressCounter.clear();
+  };
+
+  function setSelectedTerm(term){
+    $scope.progressCounter.registerTerm(term);
+    $scope.selectedTerm = term;
+    $location.search("term", term["term-english"]);
   }
-  else{
-  key = $translate.use() === "de" ? "en" : "de";
-  }
-  localStorage.setItem("language", key);
-  $translate.use(key);
-  $scope.selectedLang = key;
-  bootbox.setLocale(key);
-  moment.locale(key);
-};
-
-$scope.setSelectedItem = function(term){
-  setSelectedTerm(term.term);
-};
-
-$scope.toggleNavigation = function(){
-  $scope.navOpen = !$scope.navOpen;
-};
-
-$scope.isActive = function(path){
-  return $location.path() === path;
-};
-
-$scope.resetProgress = function(){
-  $scope.progressCounter.clear();
-};
-
-function setSelectedTerm(term){
-  $scope.progressCounter.registerTerm(term);
-  $scope.selectedTerm = term;
-  $location.search("term", term["term-english"]);
-}
 
 
-$scope.prevTerm = function(){
-  var indexOfSelectedTerm = getIndexOfSelectedTerm();
-  setSelectedTerm($scope.terms[indexOfSelectedTerm - 1]);
-};
+  $scope.prevTerm = function(){
+    var indexOfSelectedTerm = getIndexOfSelectedTerm();
+    setSelectedTerm($scope.terms[indexOfSelectedTerm - 1]);
+  };
 
-$scope.nextTerm = function(){
-  var indexOfSelectedTerm = getIndexOfSelectedTerm();
-  setSelectedTerm($scope.terms[indexOfSelectedTerm + 1]);
-};
+  $scope.nextTerm = function(){
+    var indexOfSelectedTerm = getIndexOfSelectedTerm();
+    setSelectedTerm($scope.terms[indexOfSelectedTerm + 1]);
+  };
 
-$scope.selectedTermIsFirstTerm = function(){
-  return $scope.terms[0] === $scope.selectedTerm;
-};
+  $scope.selectedTermIsFirstTerm = function(){
+    return $scope.terms[0] === $scope.selectedTerm;
+  };
 
-$scope.selectedTermIsLastTerm = function(){
-  return _.last($scope.terms) === $scope.selectedTerm;
-};
+  $scope.selectedTermIsLastTerm = function(){
+    return _.last($scope.terms) === $scope.selectedTerm;
+  };
 
 
-function getIndexOfSelectedTerm(){
-  return _.findIndex($scope.terms, function(term){
-    return $scope.selectedTerm === term;
-  });
-}
-
-function loadTermFromHash(){
-  if($location.search().term){
-    $scope.selectedTerm = _.find($scope.terms, function(term){
-      return term["term-english"] === $location.search().term || term["term-german"] === $location.search().term;
+  function getIndexOfSelectedTerm(){
+    return _.findIndex($scope.terms, function(term){
+      return $scope.selectedTerm === term;
     });
   }
-}
+
+  function loadTermFromHash(){
+    if($location.search().term){
+      $scope.selectedTerm = _.find($scope.terms, function(term){
+        return term["term-english"] === $location.search().term || term["term-german"] === $location.search().term;
+      });
+    }
+  }
 }]);
