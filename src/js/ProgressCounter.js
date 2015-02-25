97928f342b8c0fb0842f5d617a296c748501166a
Knut Hühne <knut@k-nut.eu>
2015-02-25 18:25:19 +0100

    Wrap ProgressCounter in function

--- src/js/ProgressCounter.js
+++ src/js/ProgressCounter.js
@@ -1,29 +1,31 @@
-/*jslint browser: true*/
-/*global define: false */
+var ProgressCounter = (function(){
+  "use strict";
+  function ProgressCounter(counterName, numberOfTerms){
+    this.numberOfTerms = numberOfTerms;
+    var localStorageName = counterName + ".progressCounter.readTerms";
 
-var ProgressCounter = function(counterName, numberOfTerms){
-  this.numberOfTerms = numberOfTerms;
-  var localStorageName = counterName + ".progressCounter.readTerms";
+    var readTerms = JSON.parse(localStorage.getItem(localStorageName)) || [];
 
-  var readTerms = JSON.parse(localStorage.getItem(localStorageName)) || [];
+    this.getReadTerms = function(){
+      return readTerms;
+    };
 
-  this.getReadTerms = function(){
-    return readTerms;
-  };
+    this.registerTerm = function(term){
+      if (readTerms.indexOf(term) === -1){
+        readTerms.push(term);
+      }
+      localStorage.setItem(localStorageName, JSON.stringify(readTerms));
+    };
 
-  this.registerTerm = function(term){
-    if (readTerms.indexOf(term) === -1){
-      readTerms.push(term);
-    }
-    localStorage.setItem(localStorageName, JSON.stringify(readTerms));
-  };
+    this.numberOfTermsRead = function() {
+      return readTerms.length;
+    };
 
-  this.numberOfTermsRead = function() {
-    return readTerms.length;
-  };
+    this.clear = function() {
+      localStorage.removeItem(localStorageName);
+      readTerms = [];
+    };
+  }
 
-  this.clear = function() {
-    localStorage.removeItem(localStorageName);
-    readTerms = [];
-  };
-};
+  return ProgressCounter;
+}());
