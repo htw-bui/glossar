36201a3fa0cfc42b18d78ead58d160248cc298fd
Knut Hühne <knut@k-nut.eu>
2015-02-25 18:19:07 +0100

    indentation & cleanup

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,7 @@
 (function(){
   "use strict";
 
-  moment.locale("de");
-
-  angular.module("kometFilters", ["pascalprecht.translate"]).filter("formatedTime", function() {
+  angular.module("kometFilters", []).filter("formatedTime", function() {
     return function(totalSeconds) {
       function pad(n){
         return n < 10 ? "0" + n : n;
@@ -30,11 +28,7 @@
   var fadeToggleDirective = function() {
     return {
       link: function(scope, element, attrs) {
-        scope.$watch(attrs.uiFadeToggle, function(val, oldVal) {
-          if(val === oldVal){
-            return;
-          }// Skip inital call
-          // console.log('change');
+        scope.$watch(attrs.uiFadeToggle, function() {
           element[0].animate([
             {transform: "scale3d(1, 1, 1)", offset: 0},
             {transform: "scale3d(1.5, 1.5, 1.5)", offset: 0.5},
@@ -60,70 +54,70 @@
     $translateProvider.translations("de",
       {
         NEXT: "Weiter",
-      PREV: "Zurück",
-      CONTENT: "Inhalt",
-      POINTS: "Punkte",
-      TIME: "Zeit (Ø)",
-      NAME: "Name",
-      DATE: "Datum",
-      IMPRINT: "Impressum",
-      TOP10_MESSAGE: "Sie haben es in die Top 10 geschaft! Bitte geben Sie Ihren Namen für den Highscore an",
-      FAIL_MESSAGE: "Sie haben es leider nicht in die Top10 geschafft. <br /> Probieren Sie es doch noch einmal!",
-      GLOSSARY: "Glossar",
-      GERMAN: "Deutsch",
-      ENGLISH: "Englisch"
+        PREV: "Zurück",
+        CONTENT: "Inhalt",
+        POINTS: "Punkte",
+        TIME: "Zeit (Ø)",
+        NAME: "Name",
+        DATE: "Datum",
+        IMPRINT: "Impressum",
+        TOP10_MESSAGE: "Sie haben es in die Top 10 geschaft! Bitte geben Sie Ihren Namen für den Highscore an",
+        FAIL_MESSAGE: "Sie haben es leider nicht in die Top10 geschafft. <br /> Probieren Sie es doch noch einmal!",
+        GLOSSARY: "Glossar",
+        GERMAN: "Deutsch",
+        ENGLISH: "Englisch"
       });
     $translateProvider.translations("en",
       {
         NEXT: "Next",
-      PREV: "Previous",
-      CONTENT: "Content",
-      POINTS: "Points",
-      TIME: "Time (Ø)",
-      NAME: "Name",
-      DATE: "Date",
-      IMPRINT: "Imprint",
-      TOP10_MESSAGE: "You made it into the top10! <br /> Please enter your name for the highscore.",
-      FAIL_MESSAGE: "You did not make it into the top 10. <br /> Why don't you try again?!",
-      GLOSSARY: "Glossary",
-      GERMAN: "German",
-      ENGLISH: "English"
+        PREV: "Previous",
+        CONTENT: "Content",
+        POINTS: "Points",
+        TIME: "Time (Ø)",
+        NAME: "Name",
+        DATE: "Date",
+        IMPRINT: "Imprint",
+        TOP10_MESSAGE: "You made it into the top10! <br /> Please enter your name for the highscore.",
+        FAIL_MESSAGE: "You did not make it into the top 10. <br /> Why don't you try again?!",
+        GLOSSARY: "Glossary",
+        GERMAN: "German",
+        ENGLISH: "English"
       });
     $translateProvider.preferredLanguage("en");
   });
 
   app.config(["$routeProvider", function($routeProvider){
-    $routeProvider.
-    when("/glossary", {
+    $routeProvider
+    .when("/glossary", {
       templateUrl: "templates/glossary.html",
       controller: "DashboardController",
       reloadOnSearch: false
-    }).
-  when("/highscore", {
-    templateUrl: "templates/highscore.html",
-    controller: "HighscoreCtrl"
-  }).
-  when("/game", {
-    templateUrl: "templates/game.html",
-    controller: "GameCtrl"
-  }).
-  otherwise({
-    redirectTo: "/game"
-  });
+    })
+    .when("/highscore", {
+      templateUrl: "templates/highscore.html",
+      controller: "HighscoreCtrl"
+    })
+    .when("/game", {
+      templateUrl: "templates/game.html",
+      controller: "GameCtrl"
+    })
+    .otherwise({
+      redirectTo: "/game"
+    });
   }]);
 
 
   app.directive("navigationList", function(){
     return {
       restrict: "A",
-    templateUrl: "templates/navigation-list.html"
-    };
+      templateUrl: "templates/navigation-list.html"
+      };
   });
 
   app.directive("topNavigation", function(){
     return {
       restrict: "A",
-    templateUrl: "templates/top-navigation.html"
-    };
+      templateUrl: "templates/top-navigation.html"
+      };
   });
 }());
