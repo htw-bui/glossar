ce56515b5b998eb3afeb0e63d02fa3bc7b3dc32c
Knut Hühne <knut@k-nut.eu>
2015-06-07 16:17:03 +0200

    Properly link tags (no more invalid links in links)
    See
    http://stackoverflow.com/questions/30692315/replace-text-without-escaping-in-beautifulsoup
    for an explanation of the new code

--- python/03-create-links-in-json.py
+++ python/03-create-links-in-json.py
@@ -1,10 +1,28 @@
 import json
 import re
 import os
+from bs4 import BeautifulSoup
 
 PATH_TO_DATA_FOLDER = os.path.join(os.path.dirname(__file__), '../data/')
 PATH_TO_SRC_DATA_FOLDER = os.path.join(os.path.dirname(__file__), '../src/data/')
 
+
+def tagify(html, pattern):
+    soup = BeautifulSoup(html)
+    replace = pattern
+    pattern = r'\b%s\b' % pattern
+    for txt in soup.find_all(text=True):
+        if re.search(pattern, txt, re.I) and txt.parent.name != 'a':
+            newtag = soup.new_tag('a')
+            newtag.attrs['href'] = "#/glossary?term={}".format(replace)
+            newtag.string = replace
+            txt.replace_with(newtag)
+            split_location = re.search(pattern, txt, re.IGNORECASE)
+            first_part= txt[:split_location.start()]
+            second_part = txt[split_location.end():]
+            newtag.insert_before(first_part)
+            newtag.insert_after(second_part)
+    return str(soup.p)
 
 with open(PATH_TO_SRC_DATA_FOLDER + "terms-international-with-categories.json") as infile:
     content = json.load(infile)
@@ -12,6 +30,7 @@ with open(PATH_TO_SRC_DATA_FOLDER + "terms-international-with-categories.json") 
     english_terms = [term["term-english"] for term in content]
 
 for term in content:
+    print('Current: %s' % term)
     german_def = term["description-german"]
     english_def = term["description-english"]
 
@@ -23,7 +42,7 @@ for term in content:
     allowed.remove(term["term-english"])
 
     for english_term in allowed:
-        english_linked = re.sub(r"(?<![>=])\b(%s)" % english_term, r"<a href='#/glossary?term=%s'>\1</a>" % english_term, english_linked, flags=re.IGNORECASE, count=1)
+        english_linked = tagify(english_linked, english_term)
     english_linked = english_linked.replace("\n", "<br />")
 
     # We do not want self referncing links
@@ -32,7 +51,7 @@ for term in content:
     allowed_german = sorted(allowed_german, key=len, reverse=True)
 
     for german_term in allowed_german:
-        german_linked = re.sub(r"(?<![>=])\b(%s)" % german_term, r"<a href='#/glossary?term=%s'>\1</a>" % german_term, german_linked, flags=re.IGNORECASE, count=1)
+        german_linked = tagify(german_linked, german_term)
     german_linked = german_linked.replace("\n", "<br />")
 
     term["description-german"] = german_linked
